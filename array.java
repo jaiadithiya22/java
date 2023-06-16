780e58818d3389b38eeee7a103559a0654069a7d
Jai Adithiya <jaiadithiya22@gmail.com>
2023-06-16 21:00:29 +0530

    arr

--- array.java
+++ array.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];  // Corrected the array initialization
-        numbers[0] = 1;
-        numbers[4] = 8;  // Changed index 5 to index 4 since arrays are zero-indexed
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
         System.out.println(Arrays.toString(numbers));
     }
 }
