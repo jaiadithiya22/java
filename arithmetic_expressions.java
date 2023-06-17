6f39cdbe206f39446296d0cc55bea2c4123f741e
Jai Adithiya <jaiadithiya22@gmail.com>
2023-06-17 12:12:33 +0530

    ae

---
+++ arithmetic_expressions.java
@@ -0,0 +1,12 @@
+ class Arithmetic{
+    public static void main(String[]args) {
+        int add = 10+15;
+        int sub = 10-15;
+        int multi = 10*15;
+        float div = (float)10/(float)15;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+    }
+}
